daf09757e3795a548057f72ea80a3bc0fa2017c0
Gael Grosch <{ID}+{username}@users.noreply.github.com>
2018-11-24 22:28:20 +0200

    Added live-stream option

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 git clone https://github.com/pjreddie/darknet
+cd darknet
+make
+cd ..
 wget https://pjreddie.com/media/files/yolov3.weights -O ./darknet/weights/yolov3.weights
 wget https://github.com/pjreddie/darknet/blob/master/cfg/yolov3.cfg?raw=true -O ./darknet/cfg/yolov3.cfg
 wget https://github.com/pjreddie/darknet/blob/master/data/coco.names?raw=true -O ./coco.names
--- parking-detection.py
+++ parking-detection.py
@@ -3,22 +3,24 @@
 
 # %% Imports
 import cv2 as cv
+import pafy
 import argparse
 import sys
 import numpy as np
 import os.path
 
 # %% Initialize the parameters
-confThreshold = 0.25  #Confidence threshold
+confThreshold = 0.4  #Confidence threshold
 nmsThreshold = 0.4   #Non-maximum suppression threshold
 inpWidth = 512       #Width of network's input image
 inpHeight = 512      #Height of network's input image
-frameSampling = 24
+frameSampling = 48
 bbStaticNFrame=10 # Number of epoch to wait until considering car static
 
 parser = argparse.ArgumentParser(description='Object Detection using YOLO in OPENCV')
 parser.add_argument('--image', help='Path to image file.')
 parser.add_argument('--video', help='Path to video file.')
+parser.add_argument('--youtube', help='Path to youtube url')
 parser.add_argument('--mask', help='Path to black/white mask')
 parser.add_argument('--skip', help='Skip the first X frames')
 args = parser.parse_args()
@@ -113,17 +115,18 @@ def postprocess(frame, outs, carLocationHistory, time, mask):
         height = box[3]
             
         # Debug: Print center of bb
-        cv.circle(frame, getCenterCoords(left, top, left + width, top + height), 5, (255,255,255))
+        (x_c, y_c) = getCenterCoords(left, top, left + width, top + height)
+        cv.circle(frame, (y_c, x_c), 5, (255,255,255))
 
         if isCar(classIds[i]):
-            
+
             # For now ignoring size of box, only comparing center
             seenInPreviousFrameCounter = 0
-            for i in range(1,bbStaticNFrame):
-             lastFrameBBs = [x for x in carLocationHistory if x[0] == time-i]
-             if (containSimilarBoundingBox(lastFrameBBs, left, top, left + width, top + height, width, mask)):
-                seenInPreviousFrameCounter += 1
-
+            for n in range(1,bbStaticNFrame):
+                lastFrameBBs = [x for x in carLocationHistory if x[0] == time-n]
+                if (containSimilarBoundingBox(lastFrameBBs, left, top, left + width, top + height, width, mask)):
+                    seenInPreviousFrameCounter += 1
+            
             if (seenInPreviousFrameCounter >= 0.7*bbStaticNFrame):
                 counterRecurrentCars += 1
                 drawPred(classIds[i], confidences[i], left, top, left + width, top + height, (255,255,0))
@@ -199,6 +202,18 @@ elif (args.video):
         sys.exit(1)
     cap = cv.VideoCapture(args.video)
     outputFile = args.video[:-4]+'_yolo_out_py.avi'
+elif (args.youtube):
+    # see: https://pypi.org/project/pafy/
+    url = args.youtube
+    video = pafy.new(url)
+    print("Launching video " + video.title)
+    best = video.getbest() #preftype="webm")
+    print("Url: " + best.url)
+    print("Resolution: " + best.resolution)
+    print("Extension selected: " + best.extension)
+
+    #start the video
+    cap = cv.VideoCapture(best.url)
 else:
     # Webcam input
     cap = cv.VideoCapture(0)
@@ -208,7 +223,7 @@ ret, frame = cap.read()
 height, width, channels = frame.shape
 
 # Get the video writer initialized to save the output video
-if (not args.image):
+if (not args.image or not args.youtube):
     vid_writer = cv.VideoWriter(outputFile, cv.VideoWriter_fourcc('M','J','P','G'), 30, (round(cap.get(cv.CAP_PROP_FRAME_WIDTH)),round(cap.get(cv.CAP_PROP_FRAME_HEIGHT))))
 
 if(args.skip):
@@ -240,12 +255,19 @@ while cv.waitKey(1) < 0:
 
     # Stop the program if reached end of video
     if not hasFrame:
-        print("Done processing !!!")
-        print("Output file is stored as ", outputFile)
-        cv.waitKey(3000)
-        # Release device
-        cap.release()
-        break
+        if(args.video or args.image):
+            print("Done processing !!!")
+            print("Output file is stored as ", outputFile)
+            cv.waitKey(3000)
+            # Release device
+            cap.release()
+            break
+        else:
+            # The next frame is not ready, so we try to read it again
+            cap.set(cv.cv.CV_CAP_PROP_POS_FRAMES, pos_frame-1)
+            print("frame is not ready")
+            # It is better to wait for a while for the next frame to be ready
+            cv2.waitKey(1000)
 
     # Create a 4D blob from a frame.
     blob = cv.dnn.blobFromImage(frame, 1/255, (inpWidth, inpHeight), [0,0,0], 1, crop=False)
@@ -267,7 +289,7 @@ while cv.waitKey(1) < 0:
     # Write the frame with the detection boxes
     if (args.image):
         cv.imwrite(outputFile, frame.astype(np.uint8)) #cv.bitwise_and(frame,frame,mask = mask))
-    else:
+    elif (args.video):
         vid_writer.write(frame.astype(np.uint8)) #cv.bitwise_and(frame,frame,mask = mask))
         # To stop duplicate images
         for i in range(frameSampling):
@@ -278,6 +300,13 @@ while cv.waitKey(1) < 0:
             if(not ret):
                 cap.release()
                 break
+    else:
+        #Youtube
+        for i in range(frameSampling):
+            ret = cap.grab()
+            
+        if cv.waitKey(20) & 0xFF == ord('q'):
+            break 
 
 
  
